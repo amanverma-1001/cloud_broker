9d371d0d68369f5b832a45c7ac4dd1730b673167
AMAN VERMA <ehaan1026@gmail.com>
2022-05-07 00:09:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
-const PORT=process.env.PORT || 8000;
+const PORT=process.env.port || 8000;
 
 
 app.use(cors());
